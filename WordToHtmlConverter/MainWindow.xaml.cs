b504cad16b1ac8cb5aee78d4f8be66c8614bf094
Douglas Sappet <dsappet@gmail.com>
2016-01-03 23:32:04 -0500

    Working docx to html conversion with select file box and preview window

--- WordToHtmlConverter/MainWindow.xaml.cs
+++ WordToHtmlConverter/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -27,7 +28,44 @@ namespace WordToHtmlConverter
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
+            //FileInfo file = new FileInfo(@"E:\Users\douglas\Documents\GitHub\WordToHtmlConverterTool\WordToHtmlConverter\Example.docx");
+            FileInfo file = new FileInfo(FileToConvertTextBox.Text);
 
+            if (file.Exists)
+            {
+                DirectoryInfo outputDir = new DirectoryInfo(Environment.CurrentDirectory + @"/outDir");
+                if (!outputDir.Exists)
+                {
+                    Directory.CreateDirectory(outputDir.FullName);
+                }
+
+                var htmlFile = Converter.ToHtml(file.FullName, outputDir.FullName);
+
+                var uri = new Uri(htmlFile.FullName);
+
+                PreviewWindow.Navigate(uri);
+            }
+            else
+            {
+                MessageBox.Show("That file does not exist");
+            }
+
+        }
+
+        private void FileToConvertTextBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".docx";
+            dlg.Filter = "Word Document (*.docx) |*.docx";
+            if (dlg.ShowDialog() == true)
+            {
+                FileToConvertTextBox.Text = dlg.FileName;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FileToConvertTextBox.Text = @"E:\Users\douglas\Documents\GitHub\WordToHtmlConverterTool\WordToHtmlConverter\Example.docx";
         }
     }
 }
